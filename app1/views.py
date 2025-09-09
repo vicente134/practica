c3295f41afb85d624f0e24cf16512905a68c771e
vicente134 <vicentej203@gmail.com>
2025-09-09 00:02:59 -0300

    agregado httpResponse en vistas y rutas para app1yapp2

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.urls import path
+from .import views
 # Create your views here.
+urlpatterns = [
+path('v1/', views.vista1, name='app1-v1'),
+path('v2', views.vista2, name='app2-v2'),
+]
